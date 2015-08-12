8c2726ed43c26d3aa698ae628e56386c0b3620cb
momo <github@mail.hackathon.de>
2015-08-12 12:37:10 +0200

    adding reversion

--- ihdb2/infos/admin.py
+++ ihdb2/infos/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+import reversion
 
 # Register your models here.
 from .models import Info 
 
 @admin.register(Info)
-class InfoAdmin(admin.ModelAdmin):
+class InfoAdmin(reversion.VersionAdmin):
     list_display = ('priority', 'subject', 'created')
     list_display_links = list_display
     search_fields = ('subject', 'details')
